6851c0f75e19d01c70c0ad45025255a3b2d1efe3
bulletmagnet123 <bulletmagnet.123@gmail.com>
2025-01-24 22:49:32 -0800

    got player rotating to player mouse pos and angle of thing is angled correctly

--- scenes/playerController.gd
+++ scenes/playerController.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed: float = 500
 
 func _physics_process(delta):
+	var MousePos = get_viewport().get_mouse_position()
+	
 	var direction := Vector2(
 		Input.get_action_strength("RIGHT") - Input.get_action_strength("LEFT"),
 		Input.get_action_strength("DOWN") - Input.get_action_strength("UP")
@@ -10,5 +12,9 @@ func _physics_process(delta):
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 		
+
+	var _mouse_direction = MousePos - global_position
+	var angle = _mouse_direction.angle()	
+	rotation = angle
 	velocity = direction * speed
 	move_and_slide()
