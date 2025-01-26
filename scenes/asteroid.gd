0ff9e69a59002e46af5c3467d318bab89bf24d5f
bulletmagnet123 <bulletmagnet.123@gmail.com>
2025-01-25 23:10:55 -0800

    added MineNode Animation and stuff

--- scenes/asteroid.gd
+++ scenes/asteroid.gd
@@ -1,1 +1,22 @@
 extends CharacterBody2D
+ 
+var movement_vector := Vector2()
+
+var rotation_speed: = randf_range(-1, 1) * PI / 2
+
+var speed: = randf_range(50, 100)
+#enum AsteroidSize{LARGE, MEDIUM, SMALL}
+#@export var asteroidSize := AsteroidSize
+
+func _ready():
+	rotation = randf_range(0, 2*PI)
+	print(rotation_degrees)
+	movement_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	
+	#match size:
+	#	AsteroidSize.LARGE:
+	#		speed = randf_range(50, 100)
+	#	AsteroidSize.MEDIUM:
+	#		speed = randf_range(100, 150)
+	#	AsteroidSize.SMALL:
+	#		speed = randf_range(100, 200)
