34337d168c71499a4c11656ad9af09b7f0cb4dc9
bulletmagnet123 <bulletmagnet.123@gmail.com>
2025-01-27 22:57:29 -0800

    finally got raycasting done

--- scenes/laser.gd
+++ scenes/laser.gd
@@ -1,4 +1,4 @@
-extends RayCast2D
+extends Node2D
 
 var tween
 
@@ -7,20 +7,29 @@ const RAY_LENGTH = 1000
 
 var is_active: bool
 
-@onready var Laser: RayCast2D = $RayCast2D
-@onready var cam: Camera2D = $Camera2D
+@onready var Laser: RayCast2D = $Laser
 
 var laser_color = Color(0.0235294, 1, 1, 1)
+
 func _ready() -> void:
 	tween = get_tree().create_tween()
 	is_active = true
 	var mousepos = get_local_mouse_position()
-	
-	
+	Laser.target_position = mousepos
+	Laser.modulate = laser_color
+	Laser.enabled = true
+
 func _physics_process(delta: float) -> void:
-	print(Laser.is_colliding())
-	
-	
+	if Laser:
+		if Laser.is_colliding():
+			var collider = Laser.get_collider()
+			if collider.name == "Player":  # Assuming the player's node is named "Player"
+				print("Laser is colliding with the player")
+			else:
+				print("Laser is not colliding with anything")
+	else:
+		print("Laser is not initialized")
+
 
 func activate() -> void:
 	tween.stop_all()
