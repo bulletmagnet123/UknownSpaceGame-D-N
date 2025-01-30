0cdf8c8db09318ff37005c0bbf80acd2db24d22a
bulletmagnet123 <bulletmagnet.123@gmail.com>
2025-01-29 22:22:54 -0800

    did stuff idk cant get tweens working the way i want them to

--- scenes/laser.gd
+++ scenes/laser.gd
@@ -9,33 +9,43 @@ var shoot = false
 
 @onready var line2d: Line2D = $Line2D  # Ensure the path matches the node name
 @onready var Laser: RayCast2D = $Laser
-@onready var Player: CharacterBody2D = get_parent() as CharacterBody2D # Adjust the path to the player node
+@onready var Player: CharacterBody2D = get_parent() as CharacterBody2D  # Get the parent node as Player
 @onready var collision = $Line2D/CollisionShape2D
-@onready var LaserNode = $Laser
-
 
 func _ready() -> void:
-	var player = get_node("Player")
 	tween = get_tree().create_tween()
 	var mousepos = get_local_mouse_position()
 	is_active = false
-	Player = get_tree().root.get_node("Player")  # Adjust the path to the player node
-	if player:
+	if Player:
 		Laser.add_exception(Player)  # Exclude the player from collision detection
 	else:
 		print("Player node not found")
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_shoot"):
-		is_active = true
+		if not is_active:
+			is_active = true
+			visible = true
+			tween.kill()
+			tween.tween_property(line2d, "points", [Vector2(0, 0), Vector2(beam_length, 0)], 0.5)
+			tween.set_trans(Tween.TRANS_LINEAR)
+			tween.set_ease(Tween.EASE_IN_OUT)
+			
 	else:
-		is_active = false
+		if is_active:
+			is_active = false
+			visible = false
+			tween.kill()  # Stop all tweens before starting a new one
+			tween.tween_property(line2d, "points", [Vector2(0, 0), Vector2(0, 0)], 0.5)
+			tween.set_trans(Tween.TRANS_LINEAR)
+			tween.set_ease(Tween.EASE_IN_OUT)
+			
+
 	if is_active:
 		line2d.visible = true
-		line2d.points = [Vector2(0, 0), Vector2(beam_length, 0)]
 		if Player:
 			Laser.global_position = Player.global_position  # Set laser position to player position
-			Laser.cast_to = (get_global_mouse_position() - Laser.global_position).normalized() * 10000  # Extend the laser beam infinitely towards the target
+			#Laser.cast_to = (get_global_mouse_position() - Laser.global_position).normalized() * 10000  # Extend the laser beam infinitely towards the target
 			Laser.force_raycast_update()
 			if Laser.is_colliding():
 				var collision_point = Laser.get_collision_point()
